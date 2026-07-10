0a69fa5664111dcf5d55ccb92d317ab023c1c73e
Edan Roberts <edanroberts351@gmail.com>
2026-07-10 23:31:53 +0100

    Add ASCII art representation of initials 'EOR'
    This script prints the initials 'EOR' in ASCII art format, along with a stylized representation of the letters.

---
+++ ASCIIart.py
@@ -0,0 +1,53 @@
+# Edan Oliver Roberts
+# My guitar has no headstock
+
+E = '''
+E E E E E
+E
+E
+E E E
+E
+E
+E E E E E
+'''
+
+O = '''
+  O O O  
+O       O
+O       O
+O       O
+O       O
+O       O
+  O O O  
+  '''
+
+R = '''
+R R R R  
+R       R
+R       R
+R R R R
+R   R
+R     R
+R       R
+'''
+
+
+initials = E+O+R
+
+print(initials)
+
+string1 = 'E E E E E     O O O     R R R R  '
+string2 = 'E           O       O   R       R'
+string3 = 'E           O       O   R       R'
+string4 = 'E E E       O       O   R R R R  '
+string5 = 'E           O       O   R   R    '
+string6 = 'E           O       O   R     R  '
+string7 = 'E E E E E     O O O     R       R'
+
+print(string1)
+print(string2)
+print(string3)
+print(string4)
+print(string5)
+print(string6)
+print(string7)
